94b93ac96a87fc599e0ab9a45338eff3de914245
resit.akyuz <resit.akyuz@WS107>
2017-06-28 14:09:30 +0300

    NOP 3.836 - UpsTr first phase

---
+++ Plugins/Nop.Plugin.Shipping.UPSTr/Domain/UPSCustomerClassification.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Nop.Plugin.Shipping.UPSTr.Domain
+{
+    /// <summary>
+    /// UPS customer Classification
+    /// </summary>
+    public enum UPSCustomerClassification
+    {
+        /// <summary>
+        /// Retail
+        /// </summary>
+        Retail,
+        /// <summary>
+        /// Wholesale
+        /// </summary>
+        Wholesale,
+        /// <summary>
+        /// Occasional
+        /// </summary>
+        Occasional
+    }
+}
